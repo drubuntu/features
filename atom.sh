c7236463b5c5f75a26ae9c5d22a07c15d2c5fe8d
Horvan <drubuntu@outlook.com>
2015-01-31 17:04:00 +0100

    Update atom.sh

--- atom.sh
+++ atom.sh
@@ -3,14 +3,14 @@ bdir=/opt/.drubuntu
 sudo apt-get install -y -qq git git-core build-essential libgnome-keyring-dev fakeroot npm
 git clone https://github.com/atom/atom "$bidr"
 clear
-cd /opt/.drubuntu/featurs/atom
+cd /opt/.drubuntu/atom
 git fetch -p
 git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
 if ! [ -d /src/atom  ];then
 sudo mkdir -p /src/atom
 fi
-./opt/.drubuntu/features/atom/script/build --build-dir /src/atom/
-sudo ./opt/.drubuntu/features/atom/script/grunt install  --install-dir /opt/atom
+sudo ./opt/.drubuntu/atom/script/build --build-dir /src/atom/
+sudo ./opt/.drubuntu/atom/script/grunt install  --install-dir /opt/atom
 apm install drupal-info-file
 apm install drupal
 apm install language-drupal
