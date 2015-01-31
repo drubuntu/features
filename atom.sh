bd8f785999f638f4949944cf6b82ce9b00699620
Horvan <drubuntu@outlook.com>
2015-01-31 15:48:53 +0100

    updated atom to build it from source

--- atom.sh
+++ atom.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-wget -O /opt/.drubuntu/atom-amd64.deb  https://github.com/atom/atom/releases/download/v0.166.0/atom-amd64.deb
-dpkg -i /opt/.drubuntu/atom-amd64.deb
-rm /opt/.drubuntu/atom-amd64.deb
-apt-get install -y -qq git git-core
+bdir=/opt/.drubuntu
+apt-get install -y -qq git git-core build-essential libgnome-keyring-dev fakeroot npm
+git clone https://github.com/atom/atom $bidr
+cd /opt/.drubuntu/atom
+git fetch -p
+git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+if ! [ -d /src/atom  ];then
+sudo mkdir -p /src/atom
+fi
+./opt/.drubuntu/atom/script/build --build-dir /src/atom/
+sudo ./opt/.drubuntu/atom/script/grunt install  --install-dir /opt/atom
 apm install drupal-info-file
 apm install drupal
 apm install language-drupal
