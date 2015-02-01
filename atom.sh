eab6fe007cd0d13977b62acf3fb5c324fc6b117a
Horvan <drubuntu@outlook.com>
2015-02-01 10:00:33 +0100

    Update atom.sh

--- atom.sh
+++ atom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bdir=/opt/.drubuntu
 sudo apt-get install -y -qq git git-core build-essential libgnome-keyring-dev fakeroot npm
-git clone https://github.com/atom/atom "$HOME"
+git clone https://github.com/atom/atom.git "$HOME"
 clear
 cd "$HOME"/atom
 git fetch -p
