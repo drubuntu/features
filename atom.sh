ccbdefeda31aefef70f8e0d32a1a78d91685b2fc
Horvan <drubuntu@outlook.com>
2015-02-12 09:52:52 +0100

    Update atom.sh
    fix permissions added

--- atom.sh
+++ atom.sh
@@ -1,17 +1,6 @@
 
 #!/bin/bash
-function move(){
-  sudo mv /src/atom/Atom/ /usr/share
-  mv src/atom/icons/* /usr/share/icons
-  sudo  cp -r /opt/atom/bin /usr; sudo cp -r /opt/atom/share/ /usr
-}
 
-function  build(){
-  
-cd /opt/atom
-sudo ./script/build --build-dir /src/atom
-sudo ./script/grunt install --install-dir /opt/atom
-}
 function dependencies(){
   sudo apt-get update&&sudo apt-get upgrade -y -qq
 if ! [ -f /etc/apt/sources.list.d/nodesource.list  ];then
@@ -41,6 +30,20 @@ sudo mkdir -p /src/atom
 fi
 
 }
+function  build(){
+  
+cd /opt/atom
+sudo ./script/build --build-dir /src/atom
+sudo ./script/grunt install --install-dir /opt/atom
+}
+function move(){
+  sudo mv /src/atom/Atom/ /usr/share
+  mv src/atom/icons/* /usr/share/icons
+  sudo  cp -r /opt/atom/bin /usr; sudo cp -r /opt/atom/share/ /usr
+}
+function fix_permissions(){
+  chmod -R 777 ~/.atom
+}
 function  fetch_plugins (){
 apm install drupal-info-file
 apm install drupal
@@ -58,6 +61,8 @@ function run(){
   dependencies
   clone
   build
+  move
+  fix_permissions
   fetch_plugins
   cleanup
 }
