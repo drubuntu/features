9e3922ffacd0c2c448e566aace1e42b3efd5c8a6
Horvan <drubuntu@outlook.com>
2015-02-13 10:15:09 +0100

    Update atom.sh

--- atom.sh
+++ atom.sh
@@ -3,8 +3,9 @@ deb=https://github.com/atom/atom/releases/download/v0.179.0/atom-amd64.deb
 bdir=/opt/.drubuntu
 cd "$bdir"
 wget -q "$deb" -O "bdir"/atom.deb  >>/dev/null 2&>1
-sudo dpkg -i "bdir"/atom.deb
-sudo apt-get install -f
+echo -e    "${lightgreen}Atom{NC} ${white}  ${highfin} {NC} ${ lightred} ${atombrandtext} ${NC}  ${white} ${isinstallingnowmssg} ${NC}"
+sudo dpkg -i "bdir"/atom.deb >>/dev/null 2&>1
+sudo apt-get install -y -f >>/dev/null 2&>1
 sudo dpkg -i "bdir"/atom.deb
 rm "bdir"/atom.deb
 rm --"$0"
