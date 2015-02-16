efe242909519a202aebea1e69267cc801e72c04a
Horvan <drubuntu@outlook.com>
2015-02-16 17:27:53 +0100

    Update atom.sh

--- atom.sh
+++ atom.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 deb=https://github.com/atom/atom/releases/download/v0.179.0/atom-amd64.deb
 bdir=/opt/.drubuntu
+export username=`logname`
 function  fetch_plugins (){
 apm install drupal-info-file
 apm install drupal
@@ -12,7 +13,7 @@ echo  "Downloading Atom ..."
 sudo wget "$deb" -O "$bdir"/atom.deb
 echo  "Installing Atom ..."
 sudo gdebi -n "$bdir"/atom.deb >> /dev/null 2>&1
-sudo chmod 755 ~/.atom
+sudo chmod -R 755 /home/$username/.atom
 sudo rm "bdir"/atom.deb
 echo  "get Drupal Plugins ..."
 fetch_plugins
